8cfe1bd65d9757a03b9370be67813101032cab9e
MounkoProgrammeur <jimmyprogrammeur@gmail.com>
2025-11-05 10:09:31 +0100

    okkkkkkkk

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-set -e
 
-echo "✅ Installation de pip et des dépendances Python..."
-python3 -m ensurepip --upgrade || true
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-echo "✅ Application des migrations..."
-python3 manage.py makemigrations --noinput || true
-python3 manage.py migrate --noinput || true
-
-echo "✅ Collecte des fichiers statiques..."
-python3 manage.py collectstatic --noinput --clear
+    pip install -r requirements.txt
+    python3.9 manage.py collectstatic --noinput
